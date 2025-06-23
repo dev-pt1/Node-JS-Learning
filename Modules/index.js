b8bb98ebbd7575012d2b11b372804390d65c0f32
dev-pt1 <devv.parshtechnologies@gmail.com>
2025-06-23 15:47:49 +0530

    Add files via upload

---
+++ Modules/index.js
@@ -0,0 +1,10 @@
+// const { addFn } = require("./math");
+// const { subFn } = require("./math");
+const math = require("./math")
+const { add, sub } = require("./math")
+
+console.log("Hey I am Dev!");
+
+console.log(math.add(4, 4))
+console.log(add(2, 4))
+console.log(sub(2, 4))
