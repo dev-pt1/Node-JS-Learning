76a89dc67577452fc4710d9f029e68d86733fd19
dev-pt1 <devv.parshtechnologies@gmail.com>
2025-06-23 17:37:51 +0530

    add difference of "rm" and "unlink"

--- CLI File Manger/commands/delete.js
+++ CLI File Manger/commands/delete.js
@@ -4,10 +4,20 @@ const path = require("path");
 module.exports = function rm(currentDir, fileName) {
     return new Promise((resolve, reject) => {
         const filePath = path.join(currentDir, fileName);
+
+        // fs.rm can delete file or folder
+
         fs.rm(filePath, { recursive: true, force: true }, err => {
             if (err) return reject(err);
             console.log("Deleted");
             resolve();
         });
+
+        //fs.unlink only delete file, not folder
+
+        // fs.unlink("notes.txt", err => {
+        //     if (err) throw err;
+        //     console.log("File deleted!");
+        // });
     });
 };
